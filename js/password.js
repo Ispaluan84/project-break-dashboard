bd96117996fa26cbb7ccc2693fae21ccdc29234b
Ismael Pastor <ispaluan@gmail.com>
2025-04-14 22:09:21 +0200

    Paso a paso

--- js/password.js
+++ js/password.js
@@ -16,79 +16,70 @@ const mayusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const minusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
 const simbolos = '$%&/()=?¿#[]{}<>';
+const caracteresPassword = mayusculas + minusculas + numeros + simbolos;
+
 const generatorBtn = document.getElementById('generator-btn');
 const passwordOutput = document.getElementById('password-output');
 const selector = document.getElementById('selector');
 
 
-
-function passwordGenerator () {
-const alMay = Math.floor(Math.random() * mayusculas.length)
-const leMay = mayusculas[alMay];
-
-const alMin = Math.floor(Math.random() * minusculas.length)
-const leMin = minusculas[alMin];
-
-const alNum = Math.floor(Math.random() * numeros.length);
-const leNum = numeros[alNum];
-
-const alSim = Math.floor(Math.random() * simbolos.length);
-const leSim = simbolos[alSim];
-let password = leMay + leMin + leNum + leSim;
-
-function aleatoriedad(password) {
-
-
-const min = 12;
-const max = 50;
-
-let longitudDeseada;
-if(selector && selector.value) {
-
-    longitudDeseada = parseInt(selector.value);
-    longitudDeseada = Math.max(min, Math.min(max, longitudDeseada));
-} else {
-    longitudDeseada = Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const caracteresPassword = mayusculas + minusculas + numeros + simbolos;
-
-const caracteresAdicionales = longitudDeseada - 4;
-
-
-for (let i = 0 ; i < caracteresAdicionales ; i++) {
-    const indiceAleatorio = Math.floor(Math.random() * caracteresPassword.length);
-    password += caracteresPassword.charAt(indiceAleatorio);
-}
-
-password = mezcla(password);
-
-if(passwordOutput) {
-    passwordOutput.value = password;
-} else {
-    console.log('Contraseña generada', password)
-}
+function passwordGenerator() {
+    
+    const leMay = obtenerCaracterAleatorio(mayusculas);
+    const leMin = obtenerCaracterAleatorio(minusculas);
+    const leNum = obtenerCaracterAleatorio(numeros);
+    const leSim = obtenerCaracterAleatorio(simbolos);
+    
+    
+    let password = leMay + leMin + leNum + leSim;
+    
+   
+    const min = 12;
+    const max = 50;
+    
+    let longitudDeseada;
+    if (selector && selector.value) {
+        longitudDeseada = parseInt(selector.value);
+        longitudDeseada = Math.max(min, Math.min(max, longitudDeseada));
+    } else {
+        longitudDeseada = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    
+    const caracteresAdicionales = longitudDeseada - 4;
+    for (let i = 0; i < caracteresAdicionales; i++) {
+        password += obtenerCaracterAleatorio(caracteresPassword);
+    }
+    
+    password = mezcla(password);
+    
+        passwordOutput.innerHTML =`
+        <p><strong>Su Contraseña:</strong></p> 
+        ${password}`;
 
     return password;
 }
 
+function obtenerCaracterAleatorio(cadena) {
+    const indiceAleatorio = Math.floor(Math.random() * cadena.length);
+    return cadena.charAt(indiceAleatorio);
+}
+
+
 function mezcla(cadena) {
     const arr = cadena.split('');
-    for (let i = arr.length - 1; i > 0; i--){
+    for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-    return arr.join('')
- 
+    return arr.join('');
 }
 
-aleatoriedad(password)
-}
-passwordGenerator()
+
+passwordGenerator();
+
 
 if (generatorBtn) {
-    generatorBtn.addEventListener('click', function() {
-         passwordGenerator()
-    });
+    generatorBtn.addEventListener('click', passwordGenerator);
 }
 
